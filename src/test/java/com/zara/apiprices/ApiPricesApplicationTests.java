c092885fded1d5663ae37ad99572aaa0e52714e1
Jose <elputoamo_822@hotmail.com>
2024-02-08 19:52:15 +0100

    First commit

---
+++ src/test/java/com/zara/apiprices/ApiPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zara.apiprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
